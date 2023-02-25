6212e22afebd745708809cffb88f81dd9860ca45
Lucas Rodrigues <lucasrodrigues062@gmail.com>
2023-02-25 15:20:45 -0300

    first

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,7 @@
+import { AuthService } from './auth.service';
+import { Controller } from '@nestjs/common';
+
+@Controller({})
+export class AuthController {
+  constructor(private authService: AuthService) {}
+}
